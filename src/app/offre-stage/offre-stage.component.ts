70e2ca682e299ea6c781355eae78e992c8bd76d7
Taghouti Tarek <taghoutitarek@gmail.com>
2021-03-13 16:13:34 +0100

    add count button

--- src/app/offre-stage/offre-stage.component.ts
+++ src/app/offre-stage/offre-stage.component.ts
@@ -6,16 +6,29 @@ import {Offre} from '../model/offre';
   templateUrl: './offre-stage.component.html',
   styleUrls: ['./offre-stage.component.css']
 })
+
 export class OffreStageComponent implements OnInit {
+  show: boolean;
   listOffre: Offre[];
   constructor() { }
 
   ngOnInit(): void {
     this.listOffre = [
       {ref: 'Offre 1', entreprise: 'Ent x', etat: true, titre: 'Stage'},
-      {ref: 'Offre 2', entreprise: 'Ent x', etat: true, titre: 'Stage'},
+      {ref: 'Offre 2', entreprise: 'Ent x', etat: false, titre: 'Stage'},
       {ref: 'Offre 3', entreprise: 'Ent x', etat: true, titre: 'Stage'},
     ];
+    this.show = false;
   }
 
+  bilan() {
+    let count = 0;
+    for (const i in this.listOffre) {
+      if (this.listOffre[i].etat) {
+        count++;
+      }
+    }
+    this.show = true;
+    return count;
+  }
 }
